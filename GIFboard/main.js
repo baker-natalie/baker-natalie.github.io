d2fb970a444744d9ab878cd297872d7509b8121a
Natalie Baker <baker-natalie@users.noreply.github.com>
2017-01-15 22:28:00 -0600

    refactor: fix shuffle function

--- GIFboard/main.js
+++ GIFboard/main.js
@@ -1,62 +1,37 @@
 //HTML variables
 var HTMLgifDiv = '<div class="gif"></div>';
 
-/*var visionBoard = {
+var visionBoard = {
 	gifs: [ 'url(https://media.giphy.com/media/TZKN33JOD2yQ/giphy.gif)', 'url(https://media.giphy.com/media/Vwz4zdntMXrUY/giphy.gif)', 'url(https://media.giphy.com/media/3o6Zt3AX5mSM29lGUw/giphy.gif)', 'url(https://media.giphy.com/media/l0MYIC3CENWKuZZjW/giphy.gif)', 'url(https://media.giphy.com/media/3orieVaEDcLoNbCKCQ/giphy.gif)', 'url(https://media.giphy.com/media/xT0BKhunZXlEsnpz7q/giphy.gif)'
 	]
-}*/
-var visionBoard = {
-	gifs: [
-		{
-			url : 'url(https://media.giphy.com/media/TZKN33JOD2yQ/giphy.gif)'
-		},
-		{
-			url : 'url(https://media.giphy.com/media/Vwz4zdntMXrUY/giphy.gif)'
-		},
-		{	
-			url : 'url(https://media.giphy.com/media/3o6Zt3AX5mSM29lGUw/giphy.gif)'
-		},
-		{
-			url : 'url(https://media.giphy.com/media/l0MYIC3CENWKuZZjW/giphy.gif)'
-		},
-		{
-			url : 'url(https://media.giphy.com/media/3orieVaEDcLoNbCKCQ/giphy.gif)'
-		},
-		{
-			url : 'url(https://media.giphy.com/media/xT0BKhunZXlEsnpz7q/giphy.gif)'
-		}
-	]
-};
+}
 
 visionBoard.display = function() {
-	for (var i = 0; i < visionBoard.gifs.length; i++) {
-		$(".vision-board").append(HTMLgifDiv);
-		$(".gif:last").css('background-image', visionBoard.gifs[i].url);
-	};
-};
+	this.shuffledGifs = [];
+	
+	this.shuffle = function(array) {
+  		var m = array.length, t, i;
 
-visionBoard.display();
-	
-/*var shuffle = function(array) {
-	var currentIndex= array.length, temporaryValue, randomIndex;
-	
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
+		  // While there remain elements to shuffle…
+		  while (m) {
+
+    		// Pick a remaining element…
+    		i = Math.floor(Math.random() * m--);
+
+		    // And swap it with the current element.
+		    t = array[m];
+		    array[m] = array[i];
+		    array[i] = t;
+ 		 }
+		console.log(array);
+		return array;
 	}
-	return array;
-};
-
-visionBoard.display = function() {
-	var shuffledGifs = shuffle(visionBoard.gifs);
 	
-	for (var i = 0; i < shuffledGifs.length; i++) {
+	this.shuffledGifs = this.shuffle(visionBoard.gifs);
+
+	for (var i = 0; i < this.shuffledGifs[i].length; i++) {
 		$(".vision-board").append(HTMLgifDiv);
-		$(".gif:last").css('background-image', shuffledGifs[i].url);
+		$(".gif:last").css('background-image', this.shuffledGifs[i]);
 	}
 }
-visionBoard.display();*/
+visionBoard.display();
